762b6ecb0e68e676f5a7d4cf84af5ed0aa9c8976
Azan Abrar <azan.abrar@gmail.com>
2020-09-23 18:10:11 +0500

    fixed lesson status

--- app/controllers/lesson_completion_controller.rb
+++ app/controllers/lesson_completion_controller.rb
@@ -3,44 +3,44 @@ class LessonCompletionController < ApplicationController
   def complete_lesson
     @lesson_completion = LessonCompletion.find(params[:id])
     if @lesson_completion.update_attributes!(complete: true)
-        redirect_to course_module_path(CourseModule.find(Lesson.find(@lesson_completion.lesson_id).id))
-        flash[:notice] = "Congratulations on completing that lesson!"
+      redirect_to course_module_path(@lesson_completion.lesson.course_module)
+      flash[:notice] = "Congratulations on completing that lesson!"
     else
-        redirect_to lesson_path(Lesson.find(@lesson_completion.lesson_id))
-        flash[:warning] = "Oops! Something went wrong!"
+      redirect_to lesson_path(@lesson_completion.lesson)
+      flash[:warning] = "Oops! Something went wrong!"
     end
   end
 
   def uncomplete_lesson
     @lesson_completion = LessonCompletion.find(params[:id])
     if @lesson_completion.update_attributes(complete: false)
-        redirect_to lesson_path(Lesson.find(@lesson_completion.lesson_id))
-        flash[:notice] = "Congratulations on completing that lesson!"
+      redirect_to lesson_path(@lesson_completion.lesson)
+      flash[:notice] = "Congratulations on completing that lesson!"
     else
-        redirect_to lesson_path(Lesson.find(@lesson_completion.lesson_id))
-        flash[:warning] = "Oops! Something went wrong!"
+      redirect_to lesson_path(@lesson_completion.lesson)
+      flash[:warning] = "Oops! Something went wrong!"
     end
   end
 
   def favorite_lesson
     @lesson_completion = LessonCompletion.find(params[:id])
     if @lesson_completion.update_attributes(favorite: true)
-        redirect_to lesson_path(Lesson.find(@lesson_completion.lesson_id))
-        flash[:notice] = "That lesson has been saved to your favorites."
+      redirect_to lesson_path(@lesson_completion.lesson)
+      flash[:notice] = "That lesson has been saved to your favorites."
     else
-        redirect_to lesson_path(Lesson.find(@lesson_completion.lesson_id))
-        flash[:warning] = "Oops! Something went wrong!"
+      redirect_to lesson_path(@lesson_completion.lesson)
+      flash[:warning] = "Oops! Something went wrong!"
     end
   end
 
   def unfavorite_lesson
     @lesson_completion = LessonCompletion.find(params[:id])
     if @lesson_completion.update_attributes(favorite: false)
-        redirect_to lesson_path(Lesson.find(@lesson_completion.lesson_id))
-        flash[:notice] = "That lesson has been removed from your favorites."
+      redirect_to lesson_path(@lesson_completion.lesson)
+      flash[:notice] = "That lesson has been removed from your favorites."
     else
-        redirect_to lesson_path(Lesson.find(@lesson_completion.lesson_id))
-        flash[:warning] = "Oops! Something went wrong!"
+      redirect_to lesson_path(@lesson_completion.lesson)
+      flash[:warning] = "Oops! Something went wrong!"
     end
   end
 
